9264e882b3285b486c1c242dfaf0f4356a7a9712
byanezzz <biancayanez89@gmail.com>
2017-11-02 18:05:43 -0300

    practica DOM

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function add(){
+   
+    /*Tomo el valor de lo q el usuario escribe*/
+    var comments= document.getElementById("comment").value;
+    document.getElementById("comment").value="";
+    var newComment=document.createElement("div");
+    var cont=document.getElementById("cont");
+    
+    var check=document.createElement("input");
+    check.type="checkbox";
+    
+    var paragraph=document.createElement("p");
+    paragraph.classList.add("color");
+    var nodoText=document.createTextNode(comments);
+    paragraph.appendChild(nodoText);
+    
+    var heart=document.createElement("i");
+    heart.classList.add("fa", "fa-heart", "heart");
+    
+    var trash=document.createElement("i");
+    trash.classList.add("fa", "fa-trash", "trash");
+
+    newComment.appendChild(check);
+    newComment.appendChild(trash);
+    newComment.appendChild(heart);
+    newComment.appendChild(paragraph);
+    cont.appendChild(newComment);
+
+    check.addEventListener("click", function(){
+        paragraph.classList.toggle("strike-out");
+    })
+    trash.addEventListener("click",function(){
+        cont.removeChild(newComment);
+    })
+    heart.addEventListener("click", function(){
+        heart.classList.toggle("red")
+    })
+}
